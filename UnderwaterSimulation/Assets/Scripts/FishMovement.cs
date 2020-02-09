667bd084335f4fe450f4c51c718075b3db5a3cbd
mylesing <alyafei@seas.upenn.edu>
2020-02-09 16:59:53 -0500

    spawner with init movement

---
+++ UnderwaterSimulation/Assets/Scripts/FishMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+   public int point_value;
+    public int speed;
+    public bool came_close;
+    public Vector3 end_pos;
+    public Vector3 dir;
+    public int rotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1;
+        came_close = false;
+        float ran_x = Random.Range(-1,1);
+        float ran_y = Random.Range(-1,1);
+        dir =  new Vector3(0, 0, 0);
+
+        if (ran_x == 0) 
+            ran_x = 1;
+        if(ran_y == 0)
+            ran_y = 0;
+        end_pos = new Vector3(ran_x * 50, 1, ran_y * 40);
+
+    }
+
+    void collisionEnter (Collision collision) {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (dir != Vector3.zero) {
+            transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.LookRotation(dir),
+            Time.deltaTime * rotationSpeed);
+        }
+        //random location
+        float step = speed * Time.deltaTime;
+
+        // MOVE RANDOMLY
+        if (transform.position.x > 0.1 && !came_close)
+        {
+            //if fish is far, move it towars the player
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 1, 0), step);
+        }
+        else if (transform.position.x <= 0.1 && !came_close)
+        {
+            //if the fish is at the player, keep it moving
+            //Debug.Log("I AM HERE!");
+            came_close = true;
+            dir = end_pos;
+        }
+        else if (came_close)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, end_pos, step);
+
+            //if fish is out of bouns estroy
+        }
+
+        if (transform.position.x >= 10
+            || transform.position.x <= -10
+            || transform.position.z >= 10
+            || transform.position.z <= -10) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ UnderwaterSimulation/Assets/Scripts/Spawner.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // prefabs associated with fish
+    public GameObject predator;
+    public GameObject prey;
+
+    // number of fish upon starting
+    public float num_pred;
+    public float num_prey;
+    public float growth_rate;
+    public float prey_destruction_rate;
+    public float predator_death_rate;
+    public float predator_consumption_rate;
+
+    private int timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer % 50 == 0) {
+            // clear all previous fish
+            GameObject[] allObjects = GameObject.FindGameObjectsWithTag("fish");
+            foreach(GameObject obj in allObjects) {
+                Destroy(obj);
+            }
+
+            // multiply by 10 and round up
+            float pred_count = Mathf.Round(10.0f * num_pred);
+            float prey_count = Mathf.Round(10.0f * num_prey);
+
+            // instantiate predators
+            for (int i = 0; i < pred_count; ++i) {
+                Vector3 position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+                Instantiate(predator, position, Quaternion.identity);
+            }
+
+            // instantiate prey
+            for (int i = 0; i < prey_count; ++i) {
+                Vector3 position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
+                Instantiate(prey, position, Quaternion.identity);
+            }
+
+            // test
+            Debug.Log("Predators: " + pred_count + " and Prey: " + prey_count);
+
+            // update number of predators and prey using the Lotka-Volterra equations
+            float A = growth_rate;
+            float B = prey_destruction_rate;
+            float C = predator_death_rate;
+            float D = predator_consumption_rate;
+            // dx/dt = Ax - Bxy
+            // dy/dt = -Cy + Dxy
+            // x = num prey
+            // y = num pred
+            float dx = (num_prey *  (A - B * num_pred));
+            float dy = ( -1 * num_pred) * (C - D * num_prey);
+
+            num_prey += dx;
+            num_pred += dy;
+
+            // clamping
+            if (num_prey < 0) num_prey = 0.01f;
+            if (num_prey > 10)  num_prey = 10;
+
+            if (num_pred < 0) num_pred = 0.01f;
+            if (num_pred > 10)  num_pred = 10;
+
+            // satble point
+            if (dx == 0 && dy == 0) {
+                num_pred = A / B;
+                num_prey = C / D;
+            }
+            
+        }
+
+        timer++;
+    }
+}
